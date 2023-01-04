1b3dd34eb49bfa3ec0219bf72dee4ac5f7554b06
Eficetti <ficettiesteban@gmail.com>
2023-01-04 16:25:41 -0300

    final basic changes

--- bin/barebone.dart
+++ bin/barebone.dart
@@ -1,4 +1,4 @@
-import 'package:barebone/barebone.dart' as barebone;
+import 'package:barebone/barebore.dart' as barebone;
 
 void main(List<String> arguments) {
   print('Hello world: ${barebone.calculate()}!');
--- lib/dotEnv/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:barebone/dotEnv/dot_env_example.dart';
-
-void main(List<String> arguments) {
-  // print('Hello world: ${barebone.calculate()}!');
-  var dotEnvExample = DotEnvExample();
-
-  print(dotEnvExample.getDotEnvValue());
-}
